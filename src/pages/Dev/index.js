4250fa7a2fb8e33e59f8484bbb418da952fe0db3
naeliofreires <naeliofreires@gmail.com>
2019-09-27 10:21:29 -0300

    removendo page Dev

--- src/pages/Dev/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import InputCheck from '../../components/InputCheck';
-
-export default function Dev() {
-  return <InputCheck />;
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
-import Dev from '../pages/Dev';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/dev" component={Dev} />
     </Switch>
   );
 }
